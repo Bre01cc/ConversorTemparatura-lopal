2446bd6dc5ee391821163a05ba8c83693740afd1
BRENO OLIVEIRA ASSIS REIS <25133656@cfp127aluno.local>
2025-04-23 11:24:31 -0300

    Adicionado atributos da TelaTemperatura

--- src/br/dev/Breno/ConversorDeTemperatura/gui/TelaTemperatura.java
+++ src/br/dev/Breno/ConversorDeTemperatura/gui/TelaTemperatura.java
@@ -1,8 +1,47 @@
 package br.dev.Breno.ConversorDeTemperatura.gui;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JTextArea;
 
 public class TelaTemperatura {
 	private JTextArea textCelsius;
-
+	private JLabel labelCelsius;
+	private JLabel labelResultado;
+	private JButton buttonFahreinheit;
+	private JButton buttonKelvin;
+	public JTextArea getTextCelsius() {
+		return textCelsius;
+	}
+	public void setTextCelsius(JTextArea textCelsius) {
+		this.textCelsius = textCelsius;
+	}
+	public JLabel getLabelCelsius() {
+		return labelCelsius;
+	}
+	public void setLabelCelsius(JLabel labelCelsius) {
+		this.labelCelsius = labelCelsius;
+	}
+	public JLabel getLabelResultado() {
+		return labelResultado;
+	}
+	public void setLabelResultado(JLabel labelResultado) {
+		this.labelResultado = labelResultado;
+	}
+	public JButton getButtonFahreinheit() {
+		return buttonFahreinheit;
+	}
+	public void setButtonFahreinheit(JButton buttonFahreinheit) {
+		this.buttonFahreinheit = buttonFahreinheit;
+	}
+	public JButton getButtonKelvin() {
+		return buttonKelvin;
+	}
+	public void setButtonKelvin(JButton buttonKelvin) {
+		this.buttonKelvin = buttonKelvin;
+	}
+	
+	
+	
+	
 }
--- src/br/dev/Breno/ConversorDeTemperatura/model/Temperatura.java
+++ src/br/dev/Breno/ConversorDeTemperatura/model/Temperatura.java
@@ -4,8 +4,6 @@ public class Temperatura {
 	private double celsius;
 	private double fahreinheit;
 	private double kelvin;
-	
-	
 
 	public double getCelsius() {
 		return celsius;
@@ -14,26 +12,27 @@ public class Temperatura {
 	public void setCelsius(double celsius) {
 		this.celsius = celsius;
 	}
-	
+
 	public double converterParaKelvin() {
 		kelvin = celsius + 273.15;
 		return kelvin;
-		
+
 	}
+
 	public double converterParaFahreinheit() {
-		fahreinheit = celsius* 1.8 + 32;
+		fahreinheit = celsius * 1.8 + 32;
 		return fahreinheit;
 
 	}
-	 public String resposta;
-	 
+//	 public String resposta;
+
 	public void exibirDados() {
-		System.out.println("Celsius:"+ celsius);
+		System.out.println("Celsius:" + celsius);
 		System.out.println("");
-		System.out.println("Convertando para fahreinheit:"+ fahreinheit);
+		System.out.println("Convertando para fahreinheit:" + fahreinheit);
 		System.out.println("");
-		System.out.println("Convertando para kelvin:"+ kelvin);
-		
+		System.out.println("Convertando para kelvin:" + kelvin);
+
 	}
 
 }
