eeed9a7446a635a791bf2371e1e8ce0067d569c4
BRENO OLIVEIRA ASSIS REIS <25133656@dsn127m1242314.cfp127aluno.local>
2025-04-30 11:28:43 -0300

    Mudanças no background

--- src/br/dev/Breno/ConversorDeTemperatura/gui/TelaConversor.java
+++ src/br/dev/Breno/ConversorDeTemperatura/gui/TelaConversor.java
@@ -30,13 +30,15 @@ public class TelaConversor {
 		JFrame tela = new JFrame();
 		// Criando um objeto JFrame
 		tela.setTitle(this.tituloDaTela);
-		tela.setSize(500, 400);
+		tela.setSize(420, 400);
 		// definindo o tamanho da tela
 		tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		tela.setResizable(false);
 		tela.setLayout(null);
 
 		Container container = tela.getContentPane();
+		container.setBackground(Color.blue);
+//		tela.setBackground(Color.red);
 
 		// TEXTO
 		labelCelsius = new JLabel();
@@ -52,14 +54,20 @@ public class TelaConversor {
 		// Definindo o tamanho e posição da caixa de texto celsius
 
 		// FEHRANHEIT
-		buttonFahrenheit = new JButton();// Criando objeto JButton
-		buttonFahrenheit.setText("FAHRENHEIT");// Defnindo nome do botão
-		buttonFahrenheit.setBounds(50, 100, 150, 40);// Definindo o tamanho e posição do botão Fahreinheit
+		buttonFahrenheit = new JButton();
+		// Criando objeto JButton
+		buttonFahrenheit.setText("FAHRENHEIT");
+		// Defnindo nome do botão
+		buttonFahrenheit.setBounds(50, 100, 150, 40);
+		// Definindo o tamanho e posição do botão Fahreinheit
 
 		// KELVIN
-		buttonKelvin = new JButton();// criando objeto JButton
-		buttonKelvin.setText("KELVIN");// Definindo nome do botão
-		buttonKelvin.setBounds(210, 100, 150, 40);// Definindo tamanho e posição do botão kelvin.
+		buttonKelvin = new JButton();
+		// criando objeto JButton
+		buttonKelvin.setText("KELVIN");
+		// Definindo nome do botão
+		buttonKelvin.setBounds(210, 100, 150, 40);
+		// Definindo tamanho e posição do botão kelvin.
 
 		// RESULTADOS
 		labelResultado = new JLabel();
@@ -72,12 +80,12 @@ public class TelaConversor {
 		// ERRO
 		labelMensagemErro = new JLabel();
 		// Criando objeto JLabel
-		labelMensagemErro.setForeground(Color.RED);
+		labelMensagemErro.setForeground(Color.red);
 		// Definindo a cor da mensagem de erro para vermelho
-		Font fonte = new Font("courier New", Font.ITALIC,20);
+		Font fonte = new Font("courier New", Font.ITALIC, 20);
 		labelMensagemErro.setFont(fonte);
-		//Definindo a fonte da mensagens
-		labelMensagemErro.setBounds(140, 140, 250, 60);
+		// Definindo a fonte da mensagens
+		labelMensagemErro.setBounds(100, 160, 370, 60);
 		labelMensagemErro.setVisible(false);
 		// Só estará visivel quando ocorrer um erro
 
@@ -89,10 +97,13 @@ public class TelaConversor {
 		container.add(labelResultado);
 		container.add(labelMensagemErro);
 
+		// FAHRENHEIT
 		buttonFahrenheit.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				// os comandos try e catch é parecido com if e else, try vai rodar o codigo e se
+				// não for possivel o catch vai exibir a mensagem de erro
 				try {
 					labelMensagemErro.setVisible(false);
 					// quando for valido a mensagem anterior de erro ficará invisivel
@@ -105,33 +116,42 @@ public class TelaConversor {
 					temperatura.setCelsius(celsiusLDouble);
 					// Inserindo o valor de celsius, após String ser convertida em double
 
-					if (temperatura.getCelsius() > 0) {
+					if (temperatura.getCelsius() > -274) {
+						labelResultado.setVisible(true);
+						
 						double fahreinheit = temperatura.converterParaFahrenheit();
 						labelResultado.setText(String.format("%.2f", fahreinheit) + "FAHRENHEIT");
+						
 						// criando uma variavel para armazenar o converter fahreinheit temperatura
 						// Mensagem que será exibida, tendo uma formatação nos números decimais
 					} else {
-						labelMensagemErro.setText("FAHRENHEIT INVALIDO");
+//						labelResultado.setVisible(false);
+						labelResultado.setVisible(false);
+						labelMensagemErro.setText("CELSIUS INVALIDO");
 						labelMensagemErro.setVisible(true);
+
 						// Verificando se celsius é valido para realizar a conversão se não exibir uma
 						// mensagem de erro
 					}
 
-					labelResultado.setVisible(true);// Deixando visivel o resultado
+					// Deixando visivel o resultado
 					textCelsius.requestFocus();
 
 				} catch (NumberFormatException erro) {
 					labelResultado.setVisible(false);
 					// Quando ocorrer um erro o resultado anterior ficará invisivel
-					labelMensagemErro.setVisible(true);
+
+					labelMensagemErro.setBounds(55, 140, 370, 60);
+					labelMensagemErro.setText("FORMATO INSERIDO INVALIDO");
 					// Quando os comandos no try não poderem ser executados o labelMensagemErro será
 					// visivel
+					labelMensagemErro.setVisible(true);
 
 				}
 			}
 
 		});
-
+		// KELVIN
 		buttonKelvin.addActionListener(new ActionListener() {
 
 			@Override
@@ -149,10 +169,11 @@ public class TelaConversor {
 					double kelvin = temperatura.converterParaKelvin();
 					// criando uma variavel para armazenar o converter para kelvin temperatura
 
-					if (temperatura.getCelsius() > 0) {
+					if (temperatura.getCelsius() > -274) {
 						labelResultado.setText(String.format("%.2f", kelvin) + "KELVIN");
 					} else {
-						labelMensagemErro.setText("KELVIN INVALIDO");
+						labelResultado.setVisible(false);
+						labelMensagemErro.setText("CELSIUS INVALIDO");
 						labelMensagemErro.setVisible(true);
 						// Verificando se celsius é valido para realizar a conversão se não exibir uma
 						// mensagem de erro
@@ -163,6 +184,7 @@ public class TelaConversor {
 				} catch (NumberFormatException erro) {
 					labelResultado.setVisible(false);
 					// Quando ocorrer um erro o resultado anterior ficará invisivel
+					labelMensagemErro.setBounds(55, 140, 370, 60);
 					labelMensagemErro.setText("FORMATO INSERIDO INVALIDO");
 					// Verificando se celsius é valido para realizar a conversão se não exibir uma
 					// mensagem de erro
